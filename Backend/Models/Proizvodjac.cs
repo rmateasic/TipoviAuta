a812a3bb6e818fb8ed0aa9c31a335bc813e0d3d9
Robert Mateašić <robert.mateasic@gmail.com>
2025-02-01 17:40:15 +0100

    Entitet i proizvođač

---
+++ Backend/Models/Proizvodjac.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Models
+{
+    public class Proizvodjac : Entitet
+    {
+        public string Naziv { get; set; } = "";
+        [Column(TypeName = "nvarchar(100)")]
+
+        public string Zemlja { get; set; } = "";
+
+
+    }
+}
