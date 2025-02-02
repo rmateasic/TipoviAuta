200aad9cf09041cab440f65501906565a8148784
Robert Mateašić <robert.mateasic@gmail.com>
2025-02-02 18:57:24 +0100

    Update

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -6,17 +6,21 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseSwagger();
+
+app.UseSwaggerUI();
 
 app.MapControllers();
 
