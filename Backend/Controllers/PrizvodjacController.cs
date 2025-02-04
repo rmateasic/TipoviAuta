5391d63d17f9dbc1559eb9929f5bacd6db78955f
Robert Mateašić <robert.mateasic@gmail.com>
2025-02-04 13:18:26 +0100

    Update Controllers

--- Backend/Controllers/PrizvodjacController.cs
+++ Backend/Controllers/PrizvodjacController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Backend.Controllers
 {
     [ApiController]
-    [Route("api/v1[controller]")]
+    [Route("api/v1/[controller]")]
     public class ProizvodjacController : ControllerBase
     {
         private readonly TipoviAutaContext _context;
--- Backend/Models/Proizvodjac.cs
+++ Backend/Models/Proizvodjac.cs
@@ -5,7 +5,6 @@ namespace Backend.Models
     public class Proizvodjac : Entitet
     {
         public string Naziv { get; set; } = "";
-        [Column(TypeName = "nvarchar(100)")]
 
         public string Zemlja { get; set; } = "";
 
